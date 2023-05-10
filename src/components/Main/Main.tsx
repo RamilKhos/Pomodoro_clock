b711f22fd8f0ecc04e2fb9762ff2a9ac37f4ca3d
Granit <1>
2023-05-10 19:03:57 +0400

    part logic

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,58 +3,60 @@ import { useEffect, useState } from "react"
 
 export const Main = () => {
     const [ sessionTime, setSessionTime ] = useState<any>(0)
-    const [ breakTime, setBreakTime ] = useState<any>(0)
-
-    const [hours, setHours] = useState(0)
-    const [minutes, setMinutes] = useState(0)
-
+    
+    let hours = Math.floor(sessionTime / 60)
+    let minutes = Math.floor(sessionTime - (hours * 60))
+    let seconds = sessionTime - ((hours * 60) + (minutes ))
+    
     const formatTime = (number: number) => {
         return number < 10 ? '0' + number : number
     }
+    const timer = setInterval(() => { setSessionTime(sessionTime - 1) }, 60000)
+    const btnStartHandler = () => {
+        return timer
+    }
 
     useEffect(() => {
-        if (sessionTime) {
-            let hours = Math.floor(sessionTime / 60)
-            let minutes = sessionTime - (hours * 60)
-            setHours(hours)
-            setMinutes(minutes)
-        } else {
-            setMinutes(0)
-        }
+        if (sessionTime === 0) clearInterval(timer)
+        return () => { clearInterval(timer) }
     }, [sessionTime])
 
 
     return (
         <main className="main">
-
             <div className="main__timer">
-                <div className="main__timer-title">Session</div>
+
+                <div className="main__timer-btnWrapper">
+                    <Button style={{backgroundColor: '#FAD6B1'}} size="large">
+                        Work time
+                    </Button>
+                    <Button style={{backgroundColor: '#FAD6B1'}} size="large">
+                        Break time
+                    </Button>
+                </div>
+
                 <div className="main__timer-time">
                     <span>{formatTime(hours)}</span>
                     <span>:</span>
                     <span>{formatTime(minutes)}</span>
                 </div>
+                
                 <div className="main__timer-btnWrapper">
-                    <Button className="main__timer-btn">
+                    <Button className="main__timer-btn" onClick={btnStartHandler}>
                         <i className="fa-solid fa-play"></i>
                         Start
                     </Button>
                     <Button className="main__timer-btn">
                         <i className="fa-solid fa-rotate"></i>
-                            Reset
+                        Reset
                     </Button>
                 </div>
             </div>
 
             <div className="main__timer-settings">
                 <div className="main__session-length">
-                    <h3 className="main__session-length-title">Session Length</h3>
+                    <h3 className="main__session-length-title">Session length</h3>
                     <InputNumber onChange={(e) => setSessionTime(e)} style={{ width: '60%' }} placeholder="Enter minutes"/>
-                </div>
-
-                <div className="main__break-length">
-                    <h3 className="main__break-length-title">Break Length</h3>
-                    <InputNumber onChange={(e) => setBreakTime(e)} style={{ width: '60%' }} placeholder="Enter minutes"/>
                 </div>
             </div>
 
